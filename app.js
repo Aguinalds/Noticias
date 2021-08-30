44d482fb481a7f1e36b86a33fa8940c4017290de
Aguinaldo <guimachado1708@gmail.com>
2021-08-29 21:28:47 -0300

    add consing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server');
 
-var rotasNoticias = require('./app/routes/noticias')(app);
+//var rotasNoticias = require('./app/routes/noticias')(app);
 
-var rotasHome = require('./app/routes/home')(app);
+//var rotasHome = require('./app/routes/home')(app);
 
-var rotasForm = require('./app/routes/formulario_inclusao_noticia')(app);
+//var rotasForm = require('./app/routes/formulario_inclusao_noticia')(app);
 
 
 app.listen(3000, function () {
